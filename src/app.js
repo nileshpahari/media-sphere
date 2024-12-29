61550f48abbc27b6b7de23a1dda245e0f2e0ee24
Nilesh Pahari <nileshpahari@protonmail.com>
2024-12-29 20:04:31 +0530

    add routes declaration in app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,24 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 // route importing 
-import router from "./routes/user.route.js"
+import userRouter from './routes/user.routes.js'
+import communityPostRouter from "./routes/communityPost.route.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
+//routes declaration
+app.use("/api/users", userRouter)
+app.use("/api/community-posts", communityPostRouter)
+app.use("/api/subscriptions", subscriptionRouter)
+app.use("/api/videos", videoRouter)
+app.use("/api/comments", commentRouter)
+app.use("/api/likes", likeRouter)
+app.use("/api/playlist", playlistRouter)
+app.use("/api/dashboard", dashboardRouter)
 
 
 // route declaration
